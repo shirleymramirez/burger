e0cc3df1570e167b568dddeac191206295378f21
jheilhet <shirley.mramirez@yahoo.com>
2018-02-08 21:31:22 -0700

    initial commit

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
 var express = require('express');
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 3000;
+var app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Routes
+// require("./app/routing/apiRoutes.js")(app);
+// require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
