c8654a367f957388bdc61c05457769574719d070
jheilhet <shirley.mramirez@yahoo.com>
2018-02-14 13:36:07 -0700

    added connection to JAWSDB_URL

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+// connections to JAWSDB_URL, heroku
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // local database connection
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+}
 
 connection.connect(function(err) {
     if (err) {
